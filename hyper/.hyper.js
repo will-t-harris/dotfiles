e72a9723554635d877efa257e24703d05bb681fb
Will Harris <will.t.harris@protonmail.com>
2020-04-18 18:58:40 -0700

    update zsh and hyper configs for powerline10k

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -13,7 +13,7 @@ module.exports = {
 
 		// font family with optional fallbacks
 		fontFamily:
-			'"Fira Mono for Powerline", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+			'"MesloLGS NF", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
 		// default font weight: 'normal' or 'bold'
 		fontWeight: "normal",
@@ -89,7 +89,7 @@ module.exports = {
 			lightBlue: "#6A76FB",
 			lightMagenta: "#FD7CFC",
 			lightCyan: "#68FDFE",
-			lightWhite: "#FFFFFF"
+			lightWhite: "#FFFFFF",
 		},
 
 		// the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
@@ -136,7 +136,7 @@ module.exports = {
 
 		// Whether to use the WebGL renderer. Set it to false to use canvas-based
 		// rendering (slower, but supports transparent backgrounds)
-		webGLRenderer: true
+		webGLRenderer: true,
 
 		// for advanced config flags please refer to https://hyper.is/#cfg
 	},
@@ -153,7 +153,7 @@ module.exports = {
 		"hyper-statusline",
 		"hyperborder",
 		"hyperminimal",
-		"hyper-tabs-enhanced"
+		"hyper-tabs-enhanced",
 	],
 
 	// in development, you can create a directory under
@@ -164,5 +164,5 @@ module.exports = {
 	keymaps: {
 		// Example
 		// 'window:devtools': 'cmd+alt+o',
-	}
+	},
 };
